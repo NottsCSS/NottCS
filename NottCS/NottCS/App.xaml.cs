6da586251f5f50281b20f17330caf47048e504a4
Poh Zhi Ee <poh140996@hotmail.com>
2017-12-06 19:15:55 +0800

    Add project files.

---
+++ NottCS/NottCS/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using NottCS.Views;
+using Xamarin.Forms;
+
+namespace NottCS
+{
+	public partial class App : Application
+	{
+
+		public App ()
+		{
+			InitializeComponent();
+
+
+            MainPage = new MainPage();
+        }
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ NottCS/NottCS/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using NottCS.Models;
+
+namespace NottCS.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
