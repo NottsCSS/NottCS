54ddf3d445d73b8c636512518a711a93a60d5428
Poh Zhi Ee <poh140996@hotmail.com>
2017-12-08 12:25:17 +0800

    tried to add stuff

---
+++ NottCS/NottCS/Views/MasterDetailPage1Master.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NottCS.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterDetailPage1Master : ContentPage
+    {
+        public ListView ListView;
+
+        public MasterDetailPage1Master()
+        {
+            InitializeComponent();
+
+            BindingContext = new MasterDetailPage1MasterViewModel();
+            ListView = MenuItemsListView;
+        }
+
+        class MasterDetailPage1MasterViewModel : INotifyPropertyChanged
+        {
+            public ObservableCollection<MasterDetailPage1MenuItem> MenuItems { get; set; }
+            
+            public MasterDetailPage1MasterViewModel()
+            {
+                MenuItems = new ObservableCollection<MasterDetailPage1MenuItem>(new[]
+                {
+                    new MasterDetailPage1MenuItem { Id = 0, Title = "Page 1" },
+                    new MasterDetailPage1MenuItem { Id = 1, Title = "Page 2" },
+                    new MasterDetailPage1MenuItem { Id = 2, Title = "Page 3" },
+                    new MasterDetailPage1MenuItem { Id = 3, Title = "Page 4" },
+                    new MasterDetailPage1MenuItem { Id = 4, Title = "Page 5" },
+                });
+            }
+            
+            #region INotifyPropertyChanged Implementation
+            public event PropertyChangedEventHandler PropertyChanged;
+            void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                if (PropertyChanged == null)
+                    return;
+
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+            #endregion
+        }
+    }
+}
---
+++ NottCS/NottCS/Views/Page1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Reflection;
+using NottCS.Services;
+
+namespace NottCS.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Page1 : ContentPage
+	{
+		public Page1 ()
+		{
+			InitializeComponent ();
+		    // ...
+		    // NOTE: use for debugging, not in released app code!
+		    var assembly = typeof(Page1).GetTypeInfo().Assembly;
+		    foreach (var res in assembly.GetManifestResourceNames())
+		    {
+		        System.Diagnostics.Debug.WriteLine("found resource: " + res);
+		    }
+        }
+	}
+}
