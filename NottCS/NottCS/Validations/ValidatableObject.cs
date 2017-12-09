30e1a01a15e86eab15456ca77a6ae39cba98f503
Poh Zhi Ee <poh140996@hotmail.com>
2017-12-09 12:18:42 +0800

    trying to emulate eShopOnContainers

---
+++ NottCS/NottCS/Validations/ValidatableObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NottCS.Validations
+{
+    public interface IValidatableObject<T>
+    {
+        bool Enable { get; set; }
+    }
+}
---
+++ NottCS/NottCS/ViewModels/LoginViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace NottCS.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public LoginViewModel()
+        {
+            Title = "NottCS Login";
+        }
+
+        private string _userName;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                Regex r = new Regex("^[a-zA-Z0-9]*$");
+                _userName = value;
+                if (r.IsMatch(value))
+                {
+                }
+                else
+                {
+
+                }
+            }
+        }
+        public ICommand OpenWebCommand { get; }
+    }
+}
