fea109b50afc5b639ee5a9f2fd835f00dd84ba89
Eagle Cheow <eagle.cheow@hotmail.com>
2017-12-12 21:20:31 +0800

    Added Registeration Page UI

--- NottCS/NottCS/App.xaml.cs
+++ NottCS/NottCS/App.xaml.cs
@@ -11,7 +11,7 @@ namespace NottCS
 		public App ()
 		{
 			InitializeComponent();
-            MainPage = new LoginPage();
+            MainPage = new RegisterationPage();
         }
 
 		protected override void OnStart ()
