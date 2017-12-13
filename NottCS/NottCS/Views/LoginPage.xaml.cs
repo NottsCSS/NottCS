af4e42fa34e06418dad24f728c296ddc3c97a404
Eagle Cheow <eagle.cheow@hotmail.com>
2017-12-13 19:10:40 +0800

    Fixed some UI

--- NottCS/NottCS/Views/LoginPage.xaml.cs
+++ NottCS/NottCS/Views/LoginPage.xaml.cs
@@ -15,9 +15,24 @@ namespace NottCS.Views
 		{
 			InitializeComponent ();
 		    BindingContext = viewModel = new LoginViewModel();
-		    var assembly = typeof(NottCS.Views.LoginPage);
-            if (IconOne != null) IconOne.Source = ImageSource.FromResource("NottCS.Icons.icon2.png", assembly);
-            
+		    //var assembly = typeof(NottCS.Views.LoginPage);
+            //if (IconOne != null) IconOne.Source = ImageSource.FromResource("NottCS.Images.Icons.icon2.png", assembly);
+            AddImage(IconOne, "NottCS.Images.Icons.icon2.png");
+            AddImage(BannerImage, "NottCS.Images.example-background.jpg");
+        }
+
+        /// <summary>
+        /// Applies image to the image container
+        /// </summary>
+        /// <param name="imageContainer">Name of the image container</param>
+        /// <param name="imageLocation">Image file location</param>
+        private void AddImage(Image imageContainer, string imageLocation)
+        {
+            var assembly = typeof(NottCS.Views.LoginPage);
+            if (imageContainer != null)
+            {
+                imageContainer.Source = ImageSource.FromResource(imageLocation, assembly);
+            }
         }
 	}
 }
