a7300442caf73409e0abfc5b6b7a1797150147a8
Poh Zhi-ee <poh140996@hotmail.com>
2017-12-26 22:09:25 +0800

    Added navigation from registration to registrationsuccess Changed parameter passing from login to registration to pass username Changed login username placeholder

---
+++ NottCS/NottCS/ViewModels/RegistrationSuccessViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NottCS.ViewModels
+{
+
+    class RegistrationSuccessViewModel : BaseViewModel
+    {
+        public RegistrationSuccessViewModel()
+        {
+            Title = "Registration Successful";
+        }
+    }
+}
