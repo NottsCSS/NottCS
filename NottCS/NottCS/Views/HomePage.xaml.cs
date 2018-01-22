a1572beaf148cf1c38301fa9177cebb04d1721eb
Jia Ming <jia.ming.0824@hotmail.com>
2018-01-22 12:05:52 +0800

    Renamed Home to HomePage

--- NottCS/NottCS/Views/HomePage.xaml.cs
+++ NottCS/NottCS/Views/HomePage.xaml.cs
@@ -10,16 +10,16 @@ using Xamarin.Forms.Xaml;
 namespace NottCS.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Home : ContentPage
+	public partial class HomePage : ContentPage
 	{
-		public Home ()
+		public HomePage ()
 		{
 			InitializeComponent ();
             //AddImage(Test, "NottCS.Images.Icons.icon2.png");
         }
         private void AddImage(Image imageContainer, string imageLocation)
         {
-            var assembly = typeof(NottCS.Views.LoginPage);
+            var assembly = typeof(NottCS.Views.HomePage);
             if (imageContainer != null)
             {
                 imageContainer.Source = ImageSource.FromResource(imageLocation, assembly);
