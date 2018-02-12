aa4f54c5595646fc10a458e5c4215f95c3f80931
Poh Zhi-ee <poh140996@hotmail.com>
2018-02-12 16:30:25 +0800

    added preliminary loginservice changed loginpage with preliminary android customrenderer

--- NottCS/NottCS/App.xaml.cs
+++ NottCS/NottCS/App.xaml.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using Microsoft.Identity.Client;
 using NottCS.Views;
 using Xamarin.Forms;
 
@@ -7,10 +7,12 @@ namespace NottCS
 {
 	public partial class App : Application
 	{
-
-		public App ()
+	    public static PublicClientApplication ClientApplication { get; private set; }
+	    public static readonly string[] Scopes = { "User.Read" };
+        public App ()
 		{
 			InitializeComponent();
+		    ClientApplication = new PublicClientApplication("81a5b712-2ec4-4d3f-9324-211f60d0a0c9");
             MainPage = new NavigationPage(new LoginPage());
         }
 
---
+++ NottCS/NottCS/Services/LoginService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace NottCS.Services
+{
+    internal static class LoginService
+    {
+        public static async Task<string> Authenticate()
+        {
+            try
+            {
+                AuthenticationResult ar = await App.ClientApplication.AcquireTokenAsync(App.Scopes);
+                return $"Welcome {ar.User.Name}";
+            }
+            catch (MsalException ex)
+            {
+                return ex.Message;
+            }
+
+        }
+    }
+}
