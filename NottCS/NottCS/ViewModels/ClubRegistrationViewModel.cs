9c93fc4d0990878928f2cd7f1b6fe51c5c5a948b
Jia Ming <jia.ming.0824@hotmail.com>
2018-02-12 13:05:11 +0800

    Added navigation to ClubRegistrationPage, EventPage and HomePage.

--- NottCS/NottCS/ViewModels/ClubRegistrationViewModel.cs
+++ NottCS/NottCS/ViewModels/ClubRegistrationViewModel.cs
@@ -1,10 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Windows.Input;
+using NottCS.Models;
+using Xamarin.Forms;
+using System.Threading.Tasks;
+using NottCS.Services.Navigation;
+using NottCS.Views;
 
 namespace NottCS.ViewModels
 {
-    class ClubRegistrationViewModel
+    class ClubRegistrationViewModel : BaseViewModel
     {
+        public ICommand SignUpCommand => new Command(async () => await SignUp());
+
+        private async Task SignUp()
+        {
+            try
+            {
+                await NavigationService.NavigateToAsync<QRCodeViewModel>(new QRCodePage());
+                Debug.WriteLine("Button Pressed");
+            }
+            catch (Exception e)
+            {
+
+                Debug.WriteLine(e.Message);
+            }
+
+        }
     }
 }
--- NottCS/NottCS/ViewModels/EventViewModel.cs
+++ NottCS/NottCS/ViewModels/EventViewModel.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace NottCS.ViewModels
 {
-    class EventViewModel
+    class EventViewModel : BaseViewModel
     {
     }
 }
