bc0741b9c149a14b56a07a140d536b79db84a7b5
Poh Zhi-ee <poh140996@hotmail.com>
2018-02-16 12:34:19 +0800

    Revert "updated app.xaml.cs redirecturi"
    This reverts commit ef94b2de10c049f0333c425fe302692e833eb8e3.

--- NottCS/NottCS/App.xaml.cs
+++ NottCS/NottCS/App.xaml.cs
@@ -17,7 +17,6 @@ namespace NottCS
 			InitializeComponent();
             InitializeDialogService();
 		    ClientApplication = new PublicClientApplication("81a5b712-2ec4-4d3f-9324-211f60d0a0c9");
-		    ClientApplication.RedirectUri = "msal81a5b712-2ec4-4d3f-9324-211f60d0a0c9://auth";
             MainPage = new NavigationPage(new LoginPage());
         }
 
