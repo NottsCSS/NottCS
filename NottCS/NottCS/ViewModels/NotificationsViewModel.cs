2f4d38f7acccf792dd4164ce5b52ed2660da1f93
Foong Pak Chuen <kecy6fpc@nottingham.edu.my>
2018-02-18 13:33:08 +0800

    hardcoded SettingsPage for iOS, added more text on about page, created notifications page ( partially done)

---
+++ NottCS/NottCS/ViewModels/NotificationsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Collections.Generic;
+using NottCS.Services.Navigation;
+using System;
+
+namespace NottCS.ViewModels
+{
+    internal class NotificationsViewModel: BaseViewModel
+    {
+        public class UserDataObject
+        {
+            public string Name { get; set; }
+        }
+        public List<UserDataObject> DummyLists { get; set; }
+
+        public NotificationsViewModel()
+        {
+            Debug.WriteLine("Constructor is called");
+            Title = "Notifications";
+            SetPageData();
+        }
+
+        private void SetPageData()
+        {
+            DummyLists = new List<UserDataObject>()
+            {
+                new UserDataObject() {Name = "Subscribed Clubs"},
+                new UserDataObject() {Name = "Favorite Clubs"},
+                new UserDataObject() {Name = "All Clubs"},
+            };
+        }
+    }
+}
---
+++ NottCS/NottCS/Views/NotificationsPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using NottCS.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.Diagnostics;
+
+namespace NottCS.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NotificationsPage : ContentPage
+	{
+		public NotificationsPage ()
+		{
+			InitializeComponent ();
+		    Debug.WriteLine("Attempting to call ViewModel");
+            BindingContext = new NotificationsViewModel();
+		}
+	}
+}
