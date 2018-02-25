b7d7d0d22a0a48ab20a27bd3e55c3c30d30913dc
Jia Ming <jia.ming.0824@hotmail.com>
2018-02-25 15:24:48 +0800

    Added dynamic listview to CreateEventPage. Able to delete and add text cell as needed with a minimal of 3 cell.

--- NottCS/NottCS/ViewModels/CreateEventViewModel.cs
+++ NottCS/NottCS/ViewModels/CreateEventViewModel.cs
@@ -13,36 +13,73 @@ namespace NottCS.ViewModels
 {
     class CreateEventViewModel : BaseViewModel
     {
-        public ICommand CreateTextBoxCommand => new Command(CreateTextBox);
+        public ICommand CreateTextBoxCommand => new Command(async() => await CreateTextBox());
+        public ICommand DeleteCellsCommand { get; set;}
+
         public ICommand CreateEventCommand => new Command(DoNothingForNow);
-
-        public void CreateTextBox()
+        public async Task CreateTextBox()
         {
+            try
+            {
+                ListOfTextBox.Add(new Item());
+                Debug.WriteLine("Test");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
 
+        public void DeleteTextBox()
+        {
+            if (ListOfTextBox.Count > 3)
+            {
+                ListOfTextBox.RemoveAt(ListOfTextBox.Count - 1);
+                Debug.WriteLine(ListOfTextBox.Count);
+                Debug.WriteLine("Delete command activated");
+            }
+            
         }
 
         public void DoNothingForNow()
         {
 
         }
+        
 
-        public ObservableCollection<Item> ListOfTextBox { get; set; } = new ObservableCollection<Item>()
+
+        private ObservableCollection<Item> _listOfTextBox;
+        public ObservableCollection<Item> ListOfTextBox
         {
-            new Item(),
-            new Item(),
-            new Item()
-        };
+            get => _listOfTextBox;
+            set => SetProperty(ref _listOfTextBox, value);
+        }
+        
+
+
         #region Disable ItemSelectedCommand
         public ICommand DisableItemSelectedCommand => new Command(DisableItemSelected);
         public void DisableItemSelected()
         {
         }
         #endregion
-
+        #region Disable ItemSelectedCommand1
+        public ICommand DisableItemSelectedCommand1 => new Command(DisableItemSelected1);
+        public void DisableItemSelected1()
+        {
+        }
+        #endregion
         #region CreateEventViewModel Constructor
 
         public CreateEventViewModel()
         {
+            ListOfTextBox =new ObservableCollection<Item>()
+            {
+                new Item(),
+                new Item(),
+                new Item()
+            };
+            DeleteCellsCommand = new Command(DeleteTextBox);
         }
 
         #endregion
--- NottCS/NottCS/ViewModels/HomeViewModel.cs
+++ NottCS/NottCS/ViewModels/HomeViewModel.cs
@@ -29,7 +29,7 @@ namespace NottCS.ViewModels
 
         #endregion
         #region Temporary ClubList
-
+        private ObservableCollection<Item> _clubList;
         public ObservableCollection<Item> ClubList
         {
             get => _clubList; 
@@ -151,7 +151,7 @@ namespace NottCS.ViewModels
         public string PageTitle1 { get; set; }
         public string PageTitle2 { get; set; }
         public string PageTitle3 { get; set; }
-        private ObservableCollection<Item> _clubList;
+        
         private string _selectedClubType;
 
 
@@ -253,12 +253,6 @@ namespace NottCS.ViewModels
         {
         }
         #endregion
-        #region Disable ItemSelectedCommand1
-        public ICommand DisableItemSelectedCommand1 => new Command(DisableItemSelected1);
-        public void DisableItemSelected1()
-        {
-            Debug.WriteLine("Tapped");
-        }
-        #endregion
+        
     }
 }
