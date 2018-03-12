78a705ad0a39c96b49f35aaf8b6614190fef0816
Jia Ming <jia.ming.0824@hotmail.com>
2018-03-12 19:27:06 +0800

    Added listview selecteditem highlight disable functin to test page.

--- NottCS/NottCS/Converters/SelectedItemEventArgsToSelectedItemConverter.cs
+++ NottCS/NottCS/Converters/SelectedItemEventArgsToSelectedItemConverter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
 using DLToolkit.Forms.Controls;
+using NottCS.Services;
 using Xamarin.Forms;
 
 namespace NottCS.Converters
@@ -11,12 +12,41 @@ namespace NottCS.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            DebugService.WriteLine("Selected item converter called");
             if (parameter != null && parameter is ListView)
-                if (((ListView)parameter).SelectedItem != null)
-                    ((ListView)parameter).SelectedItem = null;
+            {
+
+                if (Device.RuntimePlatform == Device.UWP)
+                {
+                    Device.BeginInvokeOnMainThread(() => ((ListView)parameter).SelectedItem = null);
+                }
+                else
+                {
+                    if (((ListView)parameter).SelectedItem != null)
+                    {
+                        ((ListView)parameter).SelectedItem = null;
+                        DebugService.WriteLine("Selected item set to null");
+                    }
+                }
+            }
+
             if (parameter != null && parameter is FlowListView)
-                if (((FlowListView)parameter).SelectedItem != null)
-                    ((FlowListView)parameter).SelectedItem = null;
+            {
+
+                if (Device.RuntimePlatform == Device.UWP)
+                {
+                    Device.BeginInvokeOnMainThread(() => ((FlowListView)parameter).SelectedItem = null);
+                }
+                else
+                {
+                    if (((FlowListView)parameter).SelectedItem != null)
+                    {
+                        ((FlowListView)parameter).SelectedItem = null;
+                        DebugService.WriteLine("Selected item set to null");
+                    }
+                }
+
+            }
 
             var eventArgs = value as SelectedItemChangedEventArgs;
             return eventArgs?.SelectedItem;
