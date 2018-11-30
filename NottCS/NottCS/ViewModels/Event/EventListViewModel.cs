5c3756c4c60e7e881548a5d020715e7f7477791f
Jia Ming Cheah <kecy6cjm@nottingham.edu.my>
2018-11-30 17:41:02 +0800

    Implemented EventService on EventList

--- NottCS/NottCS/ViewModels/Event/EventListViewModel.cs
+++ NottCS/NottCS/ViewModels/Event/EventListViewModel.cs
@@ -15,20 +15,19 @@ namespace NottCS.ViewModels.Event
     {
         private readonly INavigationService _navigationService;
         private readonly IEventService _eventService;
-        private ObservableCollection<Models.Event> _eventsList = new ObservableCollection<Models.Event>();
+        private ObservableCollection<Models.Event> _eventsLists = new ObservableCollection<Models.Event>();
 
         public EventListViewModel(INavigationService navigationService, IEventService eventService)
         {
             _navigationService = navigationService;
             _eventService = eventService;
-            var i = InitializeAsync();
         }
 
-        private async Task InitializeAsync()
-        {
-            EventsList = new ObservableCollection<Models.Event>(await _eventService.GetAllEventsAsync());
-        }
-        
+        //public ObservableCollection<Models.Event> EventList
+        //{
+        //    get => _eventLists;
+        //    set => SetProperty(ref _eventLists, value);
+        //}
         public ICommand EventListNavigationCommand => new Command(async (object p) => await EventListNavigation(p));
         private async Task EventListNavigation(object p)
         {
@@ -44,11 +43,24 @@ namespace NottCS.ViewModels.Event
                 Debug.WriteLine(e.Message);
             }
         }
-
-        public ObservableCollection<Models.Event> EventsList
+        public ObservableCollection<Models.Event> EventsList { get; set; } = new ObservableCollection<Models.Event>()
         {
-            get => _eventsList;
-            set => SetProperty(ref _eventsList, value);
-        }
+            new Models.Event()
+            {
+                Id = 0,
+                Title = "Nothing here",
+                Description = "Just nothing here",
+                EventImage = "https://blog.mozilla.org/firefox/files/2017/12/firefox-logo-600x619.png"
+            },
+            new Models.Event()
+            {
+                Id = 1,
+                Title = "Something here",
+                Description = "Just something here",
+                EventImage = "https://blog.mozilla.org/firefox/files/2017/12/firefox-logo-600x619.png"
+            },
+        };
+        
+
     }
 }
